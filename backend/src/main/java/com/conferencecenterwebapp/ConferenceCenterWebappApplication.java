d6676093ee00dee51222b1e6dc5abdeb5d2b5a91
Andrei <andreim.graur@gmail.com>
2021-12-14 10:09:12 +0200

    Main

---
+++ backend/src/main/java/com/conferencecenterwebapp/ConferenceCenterWebappApplication.java
@@ -0,0 +1,13 @@
+package com.conferencecenterwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceCenterWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceCenterWebappApplication.class, args);
+    }
+
+}
